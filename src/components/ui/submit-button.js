f121ce9cb4c0448230c800043e85a143f7d46f65
HoJae Lee <hojae.lee@softly.ai>
2025-03-24 01:14:49 +0900

    fix: remove space of submit button label

--- src/components/ui/submit-button.js
+++ src/components/ui/submit-button.js
@@ -2,9 +2,8 @@ export function SubmitButton({ label }) {
   return `
     <button
       type="submit"
+      role="link"
       class="w-full bg-blue-600 text-white p-2 rounded font-bold"
-    >
-      ${label}
-    </button>
+    >${label}</button>
   `;
 }
