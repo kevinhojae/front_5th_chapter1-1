b08804a4f7e42530ce64d9a1a90b28854e4accb8
HoJae Lee <hojae.lee@softly.ai>
2025-03-23 22:55:36 +0900

    feat: implement auth guard for router

--- src/components/browser-router.js
+++ src/components/browser-router.js
@@ -1,15 +1,24 @@
+import LoginPage from "../pages/login/page";
 import { NotFound } from "../pages/not-found";
 
 export function BrowserRouter(routes) {
   if (!Array.isArray(routes)) throw new Error("Provide routes as array");
 
   const render = () => {
+    const user = localStorage.getItem("user");
+    const isAuthenticated = !!user;
+
     const currentPath = window.location.pathname;
 
     const redirectRoute = routes.find((route) => route.path === currentPath);
 
     if (!redirectRoute) {
       document.body.innerHTML = NotFound();
+      return;
+    }
+
+    if (currentPath !== "/" && !isAuthenticated) {
+      document.body.innerHTML = LoginPage();
       return;
     }
 
