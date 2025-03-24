00c4f94242882a63c726babf1b31cdc0b57e2b81
HoJae Lee <hojae.lee@softly.ai>
2025-03-25 01:04:43 +0900

    refactor: isAuthenticated to use auth service

--- src/components/ui/nav.js
+++ src/components/ui/nav.js
@@ -1,18 +1,16 @@
+import AuthService from "../../../lib/services/auth";
 import { authService } from "../../main";
 
-// 문서 레벨에서 한 번만 이벤트 리스너 등록
 document.addEventListener("click", (e) => {
-  // 로그아웃 버튼을 클릭했는지 확인
   if (e.target && e.target.id === "logout") {
     e.preventDefault();
     authService.logout();
   }
 });
 
-export function Nav({ isAuthenticated }) {
-  // TOOO: refactor localStorage service class
+export function Nav() {
   const ProtectedLinks = () =>
-    isAuthenticated
+    AuthService.isAuthenticated
       ? `
         <li><a href="/profile" class="text-gray-600">프로필</a></li>
         <li><a id="logout" href="#" class="text-gray-600">로그아웃</a></li>
