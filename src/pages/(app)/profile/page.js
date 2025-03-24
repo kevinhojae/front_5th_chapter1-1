244a13e57d17cb28f11d7fa8cf6c7119e9eeb8a0
HoJae Lee <hojae.lee@softly.ai>
2025-03-25 01:02:54 +0900

    refactor: modularize profile logic as user service

--- src/pages/(app)/profile/page.js
+++ src/pages/(app)/profile/page.js
@@ -1,3 +1,4 @@
+import UserService from "../../../../lib/services/user";
 import { profileTemplate } from "./page.template";
 
 export default function ProfilePage(container) {
@@ -8,23 +9,14 @@ export default function ProfilePage(container) {
     const form = event.target;
     const formData = new FormData(form);
 
-    const userData = {
+    UserService.user = {
       username: formData.get("username"),
       email: formData.get("email"),
       bio: formData.get("bio"),
     };
-
-    localStorage.setItem("user", JSON.stringify(userData));
   };
 
-  const userStr = localStorage.getItem("user");
-  const user = userStr
-    ? JSON.parse(userStr)
-    : { username: "", email: "", bio: "" };
-
-  console.log("user", user);
-
-  container.innerHTML = profileTemplate(user);
+  container.innerHTML = profileTemplate(UserService.user);
 
   const profileForm = document.getElementById("profile-form");
   if (profileForm) profileForm.addEventListener("submit", handleSubmit);
