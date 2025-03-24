d86665066fd222c3971077602edeec2b37180fc7
HoJae Lee <hojae.lee@softly.ai>
2025-03-24 17:21:19 +0900

    fix: remove wrong role of submit button

--- src/components/ui/submit-button.js
+++ src/components/ui/submit-button.js
@@ -2,7 +2,6 @@ export function SubmitButton({ label }) {
   return `
     <button
       type="submit"
-      role="link"
       class="w-full bg-blue-600 text-white p-2 rounded font-bold"
     >${label}</button>
   `;
