4f8b5847bea86edd8a2c514555b3e6ecb4e7e8b9
HoJae Lee <hojae.lee@softly.ai>
2025-03-25 00:23:44 +0900

    feat: pass root element as container to pages and execute cleanup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import LandingPage from "./pages/(app)/landing/page";
 import LoginPage from "./pages/login/page";
 import ProfilePage from "./pages/(app)/profile/page";
-import { BrowserRouter } from "./components/browser-router";
+import { createBrowserRouter } from "../lib/router";
 
 const routes = [
   { path: "/", element: LandingPage },
@@ -9,46 +9,4 @@ const routes = [
   { path: "/login", element: LoginPage },
 ];
 
-export const router = BrowserRouter(routes);
-
-const rootElement = document.getElementById("root");
-
-// TODO: refactor event handler register logic
-rootElement.addEventListener("submit", (event) => {
-  const form = event.target;
-
-  if (form.getAttribute("data-form-type") === "login") {
-    event.preventDefault();
-    const formData = new FormData(form);
-
-    const username = formData.get("username");
-    // const password = formData.get("password");
-
-    localStorage.setItem(
-      "user",
-      JSON.stringify({ username, email: "", bio: "" }),
-    );
-    router.navigate("/");
-  }
-
-  if (form.getAttribute("data-form-type") === "profile") {
-    event.preventDefault();
-    const formData = new FormData(form);
-
-    const userData = {
-      username: formData.get("username"),
-      email: formData.get("email"),
-      bio: formData.get("bio"),
-    };
-
-    localStorage.setItem("user", JSON.stringify(userData));
-  }
-});
-
-rootElement.addEventListener("click", (e) => {
-  if (e.target && e.target.id === "logout") {
-    e.preventDefault();
-    localStorage.removeItem("user");
-    router.navigate("/login");
-  }
-});
+export const router = createBrowserRouter(routes);
