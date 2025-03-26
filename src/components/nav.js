931e702889f2d0e8d808986db8b1c522b2b67995
HoJae Lee <hojae.lee@softly.ai>
2025-03-27 03:29:37 +0900

    refactor: nav component to use config constant

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,27 +1,32 @@
-import AuthService from "../../lib/services/auth";
-import { authService } from "../main";
+import appContext from "@lib/contexts/app";
+import { NAV_CONFIGS } from "@lib/configs";
+import AuthService from "@lib/services/auth";
 
 document.addEventListener("click", (e) => {
   if (e.target && e.target.id === "logout") {
     e.preventDefault();
-    authService.logout();
+    appContext.authService.logout();
   }
 });
 
 export function Nav() {
-  const ProtectedLinks = () =>
-    AuthService.isAuthenticated
-      ? `
-        <li><a href="/profile" class="text-gray-600">프로필</a></li>
-        <li><a id="logout" href="#" class="text-gray-600">로그아웃</a></li>
-      `
-      : `<li><a id="login" href="/login" class="text-gray-600">로그인</a></li>`;
+  const isAuthenticated = AuthService.isAuthenticated;
+
+  const navLink = (link) => {
+    const isActive =
+      appContext.routerType === "hash"
+        ? window.location.hash === link.href
+        : window.location.pathname === link.href;
+
+    return `<li><a id="${link.id}" href="${link.href}" class="${link.className} ${isActive && "font-bold"}">${link.label}</a></li>`;
+  };
 
   return `
     <nav role="navigation" class="bg-white shadow-md p-2 sticky top-14">
       <ul class="flex justify-around">
-        <li><a href="/" class="text-blue-600">홈</a></li>
-        ${ProtectedLinks()}
+        ${NAV_CONFIGS.common.map(navLink).join("")}
+        ${isAuthenticated && NAV_CONFIGS.authenticated.map(navLink).join("")}
+        ${!isAuthenticated && NAV_CONFIGS.unauthenticated.map(navLink).join("")}
       </ul>
     </nav>
   `;
