4376b0dbb2cf8bf9525c1b367a7f397cc3925063
HoJae Lee <hojae.lee@softly.ai>
2025-03-27 03:25:35 +0900

    feat: absolute path setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vitest/config";
-
+import path from "path";
 export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@lib": path.resolve(__dirname, "./lib"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
   },
 });
