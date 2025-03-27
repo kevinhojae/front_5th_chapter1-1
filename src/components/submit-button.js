3d8833c61de70afaa23ad36462737fad39dc8041
HoJae Lee <hojae.lee@softly.ai>
2025-03-28 04:33:19 +0900

    refactor: reuse submit button for post

--- src/components/submit-button.js
+++ src/components/submit-button.js
@@ -1,8 +1,9 @@
-export function SubmitButton({ label }) {
+export function SubmitButton({ className, id, label }) {
   return `
     <button
+      id="${id}"
       type="submit"
-      class="w-full bg-blue-600 text-white p-2 rounded font-bold"
+      class="w-full bg-blue-600 text-white p-2 rounded font-bold ${className}"
     >${label}</button>
   `;
 }
--- src/pages/(app)/landing/page.template.js
+++ src/pages/(app)/landing/page.template.js
@@ -1,10 +1,10 @@
-import { PostCard } from "@components";
+import { PostCard, SubmitButton } from "@components";
 
 export const landingTemplate = ({ posts }) =>
   `
     <div class="mb-4 bg-white rounded-lg shadow p-4">
       <textarea id="post-textarea" class="w-full p-2 border rounded" placeholder="무슨 생각을 하고 계신가요?"></textarea>
-      <button id="post-button" class="mt-2 bg-blue-600 text-white px-4 py-2 rounded">게시</button>
+      ${SubmitButton({ className: "mt-2 !w-fit !px-4 hover:bg-blue-700 active:bg-blue-800", id: "post-button", label: "게시" })}
     </div>
     <div id="posts-container" class="space-y-4">
       ${posts.map((post) => PostCard({ post })).join("")}
