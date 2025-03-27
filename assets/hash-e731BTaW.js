4a1fedf2ed13f37c2daf35a78a24805ff5c07d5f
kevinhojae <kevin.hojaelee@gmail.com>
2025-03-27 20:30:53 +0900

    Updates

---
+++ assets/hash-e731BTaW.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app-CdzPrqgv.js";o.initialize("hash");console.log("In main.hash.js - appContext.routerType",o.routerType);o.router.run();
---
+++ assets/main-nkaEoNgo.js
@@ -0,0 +1,1 @@
+import{a as r}from"./app-CdzPrqgv.js";r.initialize("browser");r.router.run();
